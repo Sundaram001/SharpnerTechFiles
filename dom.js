3f965cda65a2621c821ea4b49497ae85b34ff0d2
Sundaram001 <sundaramsingh257@gmail.com>
2023-02-24 22:09:23 +0530

    initial commit

---
+++ dom.js
@@ -0,0 +1,21 @@
+
+// GETELEMENTBYCLASSNAME//
+
+ var items = document.getElementsByClassName('list-group-item');
+// items[1].textContent = "Hello 2";
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+
+
+
+//GETELEMENTBYTAGName//
+
+var li = document.getElementsByTagName('li');
+// li[1].textContent = "Hello 2";
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+
+
+
+
+
