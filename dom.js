745ac6c1f3086f0b8d8c913130d2c971ad6e3197
Sundaram001 <sundaramsingh257@gmail.com>
2023-02-24 22:56:04 +0530

    initial commit

--- dom.js
+++ dom.js
@@ -15,7 +15,20 @@ var li = document.getElementsByTagName('li');
 // li[1].style.fontWeight = 'bold';
 // li[1].style.backgroundColor = 'yellow';
 
+// QUERYSELECTOR && QuerySelectorALL //
 
+ var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = "green";
 
+ var secondItem = document.querySelector('.list-group-item:nth-child(3)');
+// secondItem.style.color = "white";
 
+//QuerySelectorALL//
 
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i = 0; i<odd.length; i++){
+    odd[i].style.backgroundColor = 'green';
+}
+
+    
