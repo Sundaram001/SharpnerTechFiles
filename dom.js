4197052ae122eed68a8d033536adbce1e70f8a52
Sundaram001 <sundaramsingh257@gmail.com>
2023-02-26 09:53:10 +0530

    initial commit

--- dom.js
+++ dom.js
@@ -27,8 +27,8 @@ var li = document.getElementsByTagName('li');
 
 var odd = document.querySelectorAll('li:nth-child(odd)');
 
-for(var i = 0; i<odd.length; i++){
-    odd[i].style.backgroundColor = 'green';
-}
+// for(var i = 0; i<odd.length; i++){
+//     odd[i].style.backgroundColor = 'green';
+// }
 
     
