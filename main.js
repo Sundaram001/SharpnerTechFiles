872d1f2e2f82b8a89d549b6e76187063108361c8
Sundaram001 <sundaramsingh257@gmail.com>
2023-02-26 09:00:00 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,38 +1,105 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
+let form = document.getElementById('addForm');
+let itemList = document.getElementById('items');
+let itemDescription = document.getElementById('description');
+let filter = document.getElementById('filter');
 
-form.addEventListener('submit', addItem);
-itemList.addEventListener('click', removeItem);
+//Function addItem
 
-function addItem(e){
+const addItem = (e) => {
     e.preventDefault();
+    
+    //Get Input Value
+    let newItem = document.getElementById('item').value;
+    // console.log(newItem);
+    //Get the input valu of Description
+    let itemDes = document.getElementById('description').value;
 
-    var newItem = document.getElementById('item').value;
+    //Create new Element
+    let li = document.createElement('li');
 
-    var li = document.createElement('li');
+    //Add Class
     li.className = 'list-group-item';
-     li.appendChild(document.createTextNode(newItem));
-     var deleteBtn = document.createElement('button');
-     var editBtn = document.createElement('button');
+    
+    //Add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+    itemList.appendChild(li);
 
-     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-     editBtn.className = 'btn btn-danger btn-sm float-right edit';
+    //Add text node with the item Description Value
+    li.appendChild(document.createElement('br'));
+     li.appendChild(document.createTextNode( itemDes));
+     itemDescription.appendChild(li);
 
-     deleteBtn.appendChild(document.createTextNode('X'));
-     editBtn.appendChild(document.createTextNode('edit'));
+    //Create Delete button
+    let delButton = document.createElement('button');
 
-     li.appendChild(deleteBtn);
-     li.appendChild(editBtn);
+    //Add Classes to del button
+    delButton.className = 'btn btn-danger btn-sm float-right delete';
 
+    //Append Text Node
+    delButton.appendChild(document.createTextNode('x'));
 
-     itemList.appendChild(li)
+    //Append Button to Li
+    li.appendChild(delButton);
+
+    //Append li to List
+    itemList.appendChild(li);
+
+    //Create an Edit Button
+    let editButton = document.createElement('button');
+    
+    //Add Classes to edit Button
+    editButton.className = 'btn btn-secondary float-right btn-sm mx-3 px-3';
+
+    //Append Text Node
+    editButton.appendChild(document.createTextNode('Edit'));
+
+    //Append button to Li
+    li.appendChild(editButton);
+
 }
 
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
-      if(confirm('Are You Sure?')){
-        var li = e.target.parentElement;
-        itemList.removeChild(li);
-      }
+//Remove Item
+const removeItem = (e) => {
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are you Sure?')){
+      let li = e.target.parentElement;
+      itemList.removeChild(li);
     }
   }
+}
+
+
+//Filter Items
+const filterItems =(e) =>{
+
+  //Convert to lower Case
+  let text = e.target.value.toLowerCase();
+  
+  //get list
+  let items = itemList.getElementsByTagName('li');
+  // console.log(items);
+  //Convert to array
+  let arr = Array.from(items);
+  console.log(arr);
+  arr.filter((item) =>{
+    let itemName = item.firstChild.textContent;
+    let itemDescription = item.firstChild.nextSibling.nextSibling.textContent;
+    // console.log(itemDescription);
+    if(itemName.toLowerCase().indexOf(text)!=-1 || itemDescription.toLowerCase().indexOf(text)!=-1){
+      item.style.display = 'block';
+    }
+    else{
+      item.style.display = 'none';
+    }
+  })
+}
+
+
+//Form submit event
+form.addEventListener('submit', addItem);
+
+//Delete Event
+itemList.addEventListener('click' , removeItem);
+
+//filter Event
+filter.addEventListener('keyup', filterItems)
